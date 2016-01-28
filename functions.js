fc8716db239cc0772bca2db4fc7893423ed493c5
heynah <hannanicol@gmail.com>
2016-01-28 21:21:40 +0000

    5 -- chose an operation switch case runs a function

--- functions.js
+++ functions.js
@@ -66,7 +66,7 @@ function sortByNameLength (sortArr) { /// modifies original array
 }
 
 var result = sortByNameLength(arr);
-*/
+
 
 //**3**
 //Create a function that can be used with Array.prototype.map. This function should 
@@ -80,3 +80,86 @@ function square(n) {
 var roots = numbers.map(square);
 
 console.log(roots)
+
+
+
+//**4**
+//Create a function that can be used with Array.prototype.map. This function 
+//should be able to take an object and square its “num” property. Then, use this 
+//function with map on an array of objects each containing a “num” property.
+
+var Dre = {
+    name: 'a',
+    num: 3,
+};
+var Andrew = {
+    name: 'diiii',
+    num: 5,
+};
+var Hanna = {
+    name: 'ciii',
+    num: 9,
+};
+
+var question4 = [Dre, Andrew, Hanna];
+
+function square(XX) {
+    return XX.num*XX.num;
+}
+var number = question4.map(square);
+
+console.log(number)
+
+//naming is to explain in "english"
+
+*/
+
+//**5**
+//In a previous workshop, you had to create a function that took two numbers and 
+//an operation (add, sub, mult, …) and returned the result of the operation on the 
+//two numbers. Here we are going to do the same but at a higher order. Create a 
+//function called operationMaker that takes only a string called operation as argument. 
+//This string could be “add”, “subtract”, “mult” or “div”. Your function will 
+//return a function that will take two numbers and return the result of running 
+//operation on these numbers. The end result should let me do something like this:
+
+
+//var adder = operationMaker(“add”); var sum = adder(5, 10); //15
+
+//var multiplier = operationMaker(“mult”); var product = mult(5, 10); // 50
+
+function operationMaker(operation) {
+ 
+    switch(operation) {
+   case 'add':
+       return function adder(a,b) {
+           return a + b;
+       }
+       
+   case 'subtract':
+       return function subtracter(a,b) {
+           return a - b;
+       }
+   
+   case 'mult':
+       return function multiplier(a,b) {
+           return a * b;
+       }
+       
+   case 'div':
+       return function divider(a,b) {
+           return a / b;
+       }
+       
+   default:
+       console.log("Bad operation!");
+    };
+    
+}
+
+console.log(operationMaker("add")(2,3));
+console.log(operationMaker("subtract")(2,3));
+console.log(operationMaker("mult")(2,3));
+console.log(operationMaker("div")(2,3));
+
+
